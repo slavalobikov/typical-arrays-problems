2693bcf361cae1a60a75924fb43bf9b35e55f8a5
lobik <lobikova12@mail.ru>
2020-10-03 18:23:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+    }else {
+        return Math.min.apply(null, array)
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+    }else {
+        return Math.max.apply(null, array)
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+    }else {
+        return array.reduce((acc, cur) => (acc + cur), 0) / array.length;
+    }
 }
